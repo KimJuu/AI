f910501be4d10f1433e4baf96d1e64ff23ea82a6
perfe <perfe@DESKTOP-H64CVFV>
2020-01-16 13:39:00 +0900

    123

--- src/newtest/Driver.java
+++ src/newtest/Driver.java
@@ -6,104 +6,132 @@ import java.io.InputStreamReader;
 
 import data.TotalData;
 
-public class Driver {
-	public static void main(String[] args) throws IOException {
+public class Driver
+{
+	public static void main(
+			String[] args)
+			throws IOException
+	{
 		NeuralNetwork neuralNetwork = new NeuralNetwork();
 		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 		boolean flag = true;
-		
-		while(flag){
-			System.out.println("학습횟수입력 * 10000");
-			int NUMB_OF_EPOCHS = Integer.parseInt(bufferedReader.readLine());
+
+		while (flag)
+		{
+			//System.out.println("학습횟수입력 * 10000");
+			//int NUMB_OF_EPOCHS = Integer.parseInt(bufferedReader.readLine());
+			int NUMB_OF_EPOCHS = 10000;
 			int back_count = 1;
-			
-			for(int i=0;i<NUMB_OF_EPOCHS * 10000;i++){
-				for(int j=0;j<TotalData.TRAININGTEST_DATA.length;j++){
-					for(int k=0;k<TotalData.TRAININGTEST_DATA[0][1].length;k++){
-						neuralNetwork.forwardprop(TotalData.TRAININGTEST_DATA[j][0]).backpropError(TotalData.TRAININGTEST_DATA[j][1][k],k);
+
+			for (int i = 0; i < NUMB_OF_EPOCHS * 10000; i++)
+			{
+				for (int j = 0; j < TotalData.TRAININGTEST_DATA.length; j++)
+				{
+					for (int k = 0; k < TotalData.TRAININGTEST_DATA[0][1].length; k++)
+					{
+						neuralNetwork.forwardprop(TotalData.TRAININGTEST_DATA[j][0]).backpropError(TotalData.TRAININGTEST_DATA[j][1][k], k);
 					}
 				}
-				if(i == back_count * 30000) {
+				if (i == back_count * 30000)
+				{
 					double[][] result = new double[TotalData.TRAININGTEST_DATA.length][TotalData.TRAININGTEST_DATA[0][1].length];
-					for(int a=0;a<TotalData.TRAININGTEST_DATA.length;a++){
-						for(int b=0;b<TotalData.TRAININGTEST_DATA[0][1].length;b++){
-							result[a][b] = neuralNetwork.forwardprop(TotalData.TRAININGTEST_DATA[a][0]).getNeurons()[b+NeuralNetwork.INPUT_NEURONS+NeuralNetwork.HIDDEN_NEURONS].getOutput();
-							}
+					for (int a = 0; a < TotalData.TRAININGTEST_DATA.length; a++)
+					{
+						for (int b = 0; b < TotalData.TRAININGTEST_DATA[0][1].length; b++)
+						{
+							result[a][b] = neuralNetwork.forwardprop(TotalData.TRAININGTEST_DATA[a][0]).getNeurons()[b + NeuralNetwork.INPUT_NEURONS + NeuralNetwork.HIDDEN_NEURONS].getOutput();
+						}
 					}
 					printResult(result);
 
 					double[][] TodayResult = new double[1][6];
-					double TRAININGTEST_DATA[][][] = new double [][][]{
-							{{2,0,1,9, 1,1,0,9, 8,8,4},{}}
+					double TRAININGTEST_DATA[][][] = new double[][][]
+					{
+						{
+							{2, 0, 2, 0, 0, 1, 1, 1, 8, 9, 3}, {} 
+							//{1, 2, 1, 4, 8, 8, 9}, {}
+						}
 					};
-					for(int j=0;j<6;j++){
-						TodayResult[0][j] = neuralNetwork.forwardprop(TRAININGTEST_DATA[0][0]).getNeurons()[j+NeuralNetwork.INPUT_NEURONS+NeuralNetwork.HIDDEN_NEURONS].getOutput();
+					for (int j = 0; j < 6; j++)
+					{
+						TodayResult[0][j] = neuralNetwork.forwardprop(TRAININGTEST_DATA[0][0]).getNeurons()[j + NeuralNetwork.INPUT_NEURONS + NeuralNetwork.HIDDEN_NEURONS].getOutput();
 					}
-					
+
 					TodayprintResult(TodayResult, TRAININGTEST_DATA);
-					
-					back_count ++;
+
+					back_count++;
 				}
 			}
 		}
 	}
 
-	static void printResult(double[][] result){
-		System.out.println("                         INPUT                          |                TARGET                |                RESULT               |             TARGET - RESULT            |");
+	static void printResult(
+			double[][] result)
+	{
+		System.out.println("                          INPUT                           |                 TARGET                 |                 RESULT                |              TARGET - RESULT             |");
 		System.out.println("-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
 
-		//input, target, output(traning value)
+		// input, target, output(traning value)
 
-		for(int i=0;i<TotalData.TRAININGTEST_DATA.length;i++){
-			//input value
-			for(int j=0;j<TotalData.TRAININGTEST_DATA[0][0].length;j++){
-				System.out.print(TotalData.TRAININGTEST_DATA[i][0][j]+"  ");	
-				if(j == (NeuralNetwork.INPUT_NEURONS -1)){
-					System.out.print(" | ");	
+		for (int i = 0; i < TotalData.TRAININGTEST_DATA.length; i++)
+		{
+			// input value
+			for (int j = 0; j < TotalData.TRAININGTEST_DATA[0][0].length; j++)
+			{
+				System.out.print(TotalData.TRAININGTEST_DATA[i][0][j] + "  ");
+				if (j == (NeuralNetwork.INPUT_NEURONS - 1))
+				{
+					System.out.print(" |   ");
 				}
 			}
-			
-			//target value
-			for(int k=0;k<TotalData.TRAININGTEST_DATA[0][1].length;k++){
-				System.out.print(TotalData.TRAININGTEST_DATA[i][1][k] + "  ");		
-				if(k == (NeuralNetwork.OUTPUT_NEURONS -1)){
-					System.out.print(" | ");
+
+			// target value
+			for (int k = 0; k < TotalData.TRAININGTEST_DATA[0][1].length; k++)
+			{
+				System.out.print(TotalData.TRAININGTEST_DATA[i][1][k] + "  ");
+				if (k == (NeuralNetwork.OUTPUT_NEURONS - 1))
+				{
+					System.out.print(" |   ");
 				}
 			}
-			
-			//output value(traning value)
-			for(int p=0;p<6;p++){
-				System.out.print(String.format("%.2f", result[i][p])+ "  ");		
+
+			// output value(traning value)
+			for (int p = 0; p < 6; p++)
+			{
+				System.out.print(String.format("%.2f", result[i][p]) + "  ");
 			}
-			System.out.print(" | ");
-			
-			//target - result value
-			for(int target=0; target<TotalData.TRAININGTEST_DATA[0][1].length; target++){
-				if(Math.abs(TotalData.TRAININGTEST_DATA[i][1][target] - result[i][target]) > 0.01) {
-					System.out.print("`" + String.format("%.2f", TotalData.TRAININGTEST_DATA[i][1][target] - result[i][target]) + "  ");
-				}else {
-					System.out.print(String.format("%.2f", TotalData.TRAININGTEST_DATA[i][1][target] - result[i][target]) + "  ");
-				}
+			System.out.print(" |   ");
+
+			// target - result value
+			for (int target = 0; target < TotalData.TRAININGTEST_DATA[0][1].length; target++)
+			{
+				System.out.print(String.format("%.2f", Math.abs(TotalData.TRAININGTEST_DATA[i][1][target] - result[i][target])) + "  ");
 			}
-			
+
 			System.out.println("");
 		}
 	}
-	
-	static void TodayprintResult(double[][] result, double[][][] data){
-		System.out.println("                         INPUT                          |                RESULT                |");
+
+	static void TodayprintResult(
+			double[][] result,
+			double[][][] data)
+	{
+		System.out.println("                          INPUT                           |                 RESULT                 |");
 		System.out.println("------------------------------------------------------------------------------------------------");
 
-			for(int j=0;j<TotalData.TRAININGTEST_DATA[0][0].length;j++){
-				System.out.print(data[0][0][j]+"  ");	// next week input
-				if(j == (NeuralNetwork.INPUT_NEURONS -1)){
-					System.out.print(" | ");	
-				}
+		for (int j = 0; j < TotalData.TRAININGTEST_DATA[0][0].length; j++)
+		{
+			System.out.print(data[0][0][j] + "  ");	// next week input
+			if (j == (NeuralNetwork.INPUT_NEURONS - 1))
+			{
+				System.out.print(" |   ");
 			}
-			for(int p=0;p<6;p++){
-				System.out.print(String.format("%.2f", result[0][p])+ "  ");		//next week output
-			}
-			System.out.println("");
+		}
+		for (int p = 0; p < 6; p++)
+		{
+			System.out.print(String.format("%.2f", result[0][p]) + "  ");		// next week output
+		}
+		System.out.println("");
 	}
-	
+
 }
--- src/newtest/NeuralNetwork.java
+++ src/newtest/NeuralNetwork.java
@@ -2,84 +2,114 @@ package newtest;
 
 import java.util.Arrays;
 
+public class NeuralNetwork
+{
 
-public class NeuralNetwork {
-	
-	static enum LayerType {I, H, O}
-	static final double LEARNING_RATE=0.015;
+	static enum LayerType
+	{
+		I, H, O
+	}
+
+	static final double LEARNING_RATE = 0.017345345;
 	final static int INPUT_NEURONS = 11;
 	final static int HIDDEN_NEURONS = 18;
 	final static int OUTPUT_NEURONS = 6;
-	private Neuron[] neurons = new Neuron[INPUT_NEURONS + HIDDEN_NEURONS +OUTPUT_NEURONS];
-	public NeuralNetwork(){	//뉴런생성
-		for(int i=0;i<INPUT_NEURONS;i++) neurons[i] = new Neuron(LayerType.I);
-		for(int i=INPUT_NEURONS;i<INPUT_NEURONS + HIDDEN_NEURONS;i++) neurons[i] = new Neuron(LayerType.H);
-		for(int i=INPUT_NEURONS + HIDDEN_NEURONS;i<INPUT_NEURONS + HIDDEN_NEURONS + OUTPUT_NEURONS ;i++) neurons[i] = new Neuron(LayerType.O);
+	private Neuron[] neurons = new Neuron[INPUT_NEURONS + HIDDEN_NEURONS + OUTPUT_NEURONS];
+
+	public NeuralNetwork()
+	{	// 뉴런생성
+		for (int i = 0; i < INPUT_NEURONS; i++)
+			neurons[i] = new Neuron(LayerType.I);
+		for (int i = INPUT_NEURONS; i < INPUT_NEURONS + HIDDEN_NEURONS; i++)
+			neurons[i] = new Neuron(LayerType.H);
+		for (int i = INPUT_NEURONS + HIDDEN_NEURONS; i < INPUT_NEURONS + HIDDEN_NEURONS + OUTPUT_NEURONS; i++)
+			neurons[i] = new Neuron(LayerType.O);
 	}
-	
-	public NeuralNetwork forwardprop(double input[]){
-		double weightedSum=0;
-		for (int i = 0; i < neurons.length; i++) {
-			switch (neurons[i].getLayerType()) {
-			case I:
-				neurons[i].setOutput(input[i]);
-				break;
-			case H:
-				weightedSum = neurons[i].getThreshold();
-				for(int InputNo=0;InputNo<INPUT_NEURONS; InputNo++){
-					weightedSum += neurons[i].getWeights()[InputNo] * neurons[InputNo].getOutput();
-				}
-				neurons[i].applyActivationFunction(weightedSum);
-				break;
-			case O:
-				weightedSum = neurons[i].getThreshold();
-				for(int HiddenNo= INPUT_NEURONS; HiddenNo<INPUT_NEURONS + HIDDEN_NEURONS;HiddenNo++){
-					weightedSum += neurons[i].getWeights()[HiddenNo - INPUT_NEURONS] * neurons[HiddenNo].getOutput();
-				}
-				neurons[i].applyActivationFunction(weightedSum);
-				break;
+
+	public NeuralNetwork forwardprop(
+			double input[])
+	{
+		double weightedSum = 0;
+		for (int i = 0; i < neurons.length; i++)
+		{
+			switch (neurons[i].getLayerType())
+			{
+				case I:
+					neurons[i].setOutput(input[i]);
+					break;
+				case H:
+					weightedSum = neurons[i].getThreshold();
+					for (int InputNo = 0; InputNo < INPUT_NEURONS; InputNo++)
+					{
+						weightedSum += neurons[i].getWeights()[InputNo] * neurons[InputNo].getOutput();
+					}
+					neurons[i].applyActivationFunction(weightedSum);
+					break;
+				case O:
+					weightedSum = neurons[i].getThreshold();
+					for (int HiddenNo = INPUT_NEURONS; HiddenNo < INPUT_NEURONS + HIDDEN_NEURONS; HiddenNo++)
+					{
+						weightedSum += neurons[i].getWeights()[HiddenNo - INPUT_NEURONS] * neurons[HiddenNo].getOutput();
+					}
+					neurons[i].applyActivationFunction(weightedSum);
+					break;
 			}
 		}
 		return this;
 	}
-	
-	public NeuralNetwork backpropError(double targetResult, int outputNo){
-		
-		//OUTPUT
-		for(int i=INPUT_NEURONS + HIDDEN_NEURONS;i<INPUT_NEURONS + HIDDEN_NEURONS + OUTPUT_NEURONS;i++){
-			//OUTPUT SET
-			if(i == (outputNo+ INPUT_NEURONS + HIDDEN_NEURONS)){
+
+	public NeuralNetwork backpropError(
+			double targetResult,
+			int outputNo)
+	{
+
+		// OUTPUT
+		for (int i = INPUT_NEURONS + HIDDEN_NEURONS; i < INPUT_NEURONS + HIDDEN_NEURONS + OUTPUT_NEURONS; i++)
+		{
+			// OUTPUT SET
+			if (i == (outputNo + INPUT_NEURONS + HIDDEN_NEURONS))
+			{
 				neurons[i].setError((targetResult - neurons[i].getOutput()) * neurons[i].derivative());
 				neurons[i].setThreshold(neurons[i].getThreshold() + LEARNING_RATE * neurons[i].getError());
-				
-				for(int j=INPUT_NEURONS;j<INPUT_NEURONS + HIDDEN_NEURONS;j++){
-						neurons[i].getWeights()[INPUT_NEURONS + HIDDEN_NEURONS - j-1] = neurons[i].getWeights()[INPUT_NEURONS + HIDDEN_NEURONS - j - 1] + 
-								LEARNING_RATE * neurons[i].getError() * neurons[(HIDDEN_NEURONS + INPUT_NEURONS -1) - j+INPUT_NEURONS ].getOutput();
+
+				for (int j = INPUT_NEURONS; j < INPUT_NEURONS + HIDDEN_NEURONS; j++)
+				{
+					neurons[i].getWeights()[INPUT_NEURONS + HIDDEN_NEURONS - j - 1] = neurons[i].getWeights()[INPUT_NEURONS + HIDDEN_NEURONS - j - 1] + LEARNING_RATE * neurons[i].getError() * neurons[(HIDDEN_NEURONS + INPUT_NEURONS - 1) - j + INPUT_NEURONS].getOutput();
 				}
 			}
 		}
-		
-		//HIDDEN
-		for(int i=INPUT_NEURONS;i<INPUT_NEURONS + HIDDEN_NEURONS;i++){
 
-			//HIDDEN ERROR SUM
-			double ErrorSum = 0; 
-			for(int j=INPUT_NEURONS + HIDDEN_NEURONS;j<INPUT_NEURONS + HIDDEN_NEURONS + OUTPUT_NEURONS;j++){
-				ErrorSum += (neurons[j].getWeights()[i-INPUT_NEURONS] * neurons[j].getError());
+		// HIDDEN
+		for (int i = INPUT_NEURONS; i < INPUT_NEURONS + HIDDEN_NEURONS; i++)
+		{
+
+			// HIDDEN ERROR SUM
+			double ErrorSum = 0;
+			for (int j = INPUT_NEURONS + HIDDEN_NEURONS; j < INPUT_NEURONS + HIDDEN_NEURONS + OUTPUT_NEURONS; j++)
+			{
+				ErrorSum += (neurons[j].getWeights()[i - INPUT_NEURONS] * neurons[j].getError());
 			}
-			
-			//HIDDEN SET
+
+			// HIDDEN SET
 			neurons[i].setError(ErrorSum * neurons[i].derivative());
 			neurons[i].setThreshold(neurons[i].getThreshold() + LEARNING_RATE * neurons[i].getError());
-			for(int k=0;k<INPUT_NEURONS;k++){
+			for (int k = 0; k < INPUT_NEURONS; k++)
+			{
 				neurons[i].getWeights()[k] = neurons[i].getWeights()[k] + LEARNING_RATE * neurons[i].getError() * neurons[k].getOutput();
 			}
 		}
-		
+
 		return this;
 	}
-	
-	public Neuron[] getNeurons() {return neurons;}	
-	public String toString() {return Arrays.toString(neurons);}
-	
+
+	public Neuron[] getNeurons()
+	{
+		return neurons;
+	}
+
+	public String toString()
+	{
+		return Arrays.toString(neurons);
+	}
+
 }
